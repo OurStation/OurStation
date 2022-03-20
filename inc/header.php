afde2e31f0d5edf7b2f67f6db0760ad0789bc60f
ShockAwer <55027860+ShockAwer@users.noreply.github.com>
2022-03-20 14:41:39 -0400

    Update and rename index.php to header.php

---
+++ inc/header.php
@@ -0,0 +1,69 @@
+<link href="styles.css" rel="stylesheet" type="text/css">
+<table align="center" width="100%" bgcolor="#D5E5F5" cellpadding="0" cellspacing="0" border="0">
+	<tbody><tr>
+		<td><img src="http://www.youtube.com/img/box_login_tl.gif" width="5" height="5"></td>
+		<td><img src="http://www.youtube.com/img/pixel.gif" width="1" height="5"></td>
+		<td><img src="http://www.youtube.com/img/box_login_tr.gif" width="5" height="5"></td>
+	</tr>
+	<tr>
+		<td><img src="http://www.youtube.com/img/pixel.gif" width="5" height="1"></td>
+		
+		<td width="100%">
+
+		<table width="100%" cellpadding="0" cellspacing="0" border="0">
+				<tbody><tr>
+					<td>
+					<table cellpadding="2" cellspacing="0" border="0">
+						<tbody><tr>
+							<td>&nbsp;<a href="index.php" class="bold">Home</a></td>
+							<td>&nbsp;|&nbsp;</td>
+							<td><a href="my_videos.php">My Videos</a></td>
+							<td>&nbsp;|&nbsp;</td>
+							<td><a href="my_favorites.php">My Favorites</a></td>
+							<td>&nbsp;|&nbsp;</td>
+							<td><a href="my_messages.php">My Messages</a>
+							</td>
+							<td>&nbsp;|&nbsp;</td>
+							<td><a href="my_profile.php">My Profile</a></td>
+						</tr>
+					</tbody></table>
+					</td>
+				</tr>
+			</tbody></table>
+			
+			</td>
+	
+		<td><img src="http://www.youtube.com/img/pixel.gif" width="5" height="1"></td>
+	</tr>
+	<tr>
+		<td><img src="http://www.youtube.com/img/box_login_bl.gif" width="5" height="5"></td>
+		<td><img src="http://www.youtube.com/img/pixel.gif" width="1" height="5"></td>
+		<td><img src="http://www.youtube.com/img/box_login_br.gif" width="5" height="5"></td>
+	</tr>
+</tbody></table>
+
+<div class="tableLinkBar">
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+	<tbody><tr>
+	
+			
+		<td width="100%" align="right">
+		<table align="right" cellpadding="2" cellspacing="0" border="0">
+			<tbody><tr>
+							<td><a href="signup.php" class="bold">Sign Up</a></td>
+				<td>&nbsp;|&nbsp;</td>
+				<td><a href="login.php">Log In</a></td>
+				<td>&nbsp;|&nbsp;</td>
+				<td><a href="help.php">Help</a>&nbsp;</td>
+						</tr>
+		</tbody></table>
+		</td>
+	</tr>
+
+		
+</tbody></table></div>
+
+	
+		<td align="center" valign="center"><span class="footer"><a href="about.php">About Us</a> | <a href="contact.php">Contact Us</a> | <a href="terms.php">Terms of Use</a> | <a href="privacy.php">Privacy Policy</a> | Copyright (C) 2005 OurStation, LLC | <a href="recently_added.rss"><img src="RSS.gif" width="36" height="14" border="0" style="vertical-align: text-top;"></a></span></td>
+	</tr>
+</tbody></table>
